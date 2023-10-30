758267b2589853fda1896699b4f259f52fd823eb
Steven Clontz <steven.clontz@gmail.com>
2023-10-30 17:03:42 +0000

    move login page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,24 +1,6 @@
-<!-- src/routes/+page.svelte -->
 <script lang="ts">
-	import { Auth } from '@supabase/auth-ui-svelte'
-	import { ThemeSupa } from '@supabase/auth-ui-shared'
-
-	export let data
 </script>
 
-<h3>STDC2024 Abstract Submission</h3>
 <p>
-	To submit or edit your abstract, enter your email address below and click "Send Magic Link".
-	Use the link that is emailed to you to access your abstract.
+	<a href="/login">Log in</a>
 </p>
-<div class="row flex-center flex">
-	<div class="col-6 form-widget">
-		<Auth
-			supabaseClient={data.supabase}
-			view="magic_link"
-			redirectTo={`${data.url}/auth/callback`}
-			showLinks={false}
-			appearance={{ theme: ThemeSupa, style: { input: 'color: #000' } }}
-		/>
-	</div>
-</div>

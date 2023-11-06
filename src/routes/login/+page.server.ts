77c697b0bc6a22bb725ec9b053fd461328ef812b
Steven Clontz <steven.clontz@gmail.com>
2023-11-06 17:04:24 +0000

    reroute to home for login

--- src/routes/login/+page.server.ts
+++ src/routes/login/+page.server.ts
@@ -5,7 +5,7 @@ export const load = async ({ url, locals: { getSession } }) => {
 
 	// if the user is already logged in return them to the account page
 	if (session) {
-		throw redirect(303, '/profile')
+		throw redirect(303, '/')
 	}
 
 	return

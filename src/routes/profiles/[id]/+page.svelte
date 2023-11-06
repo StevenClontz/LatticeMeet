be05111568c78f670d49aaab869d55a6ec021108
Steven Clontz <steven.clontz@gmail.com>
2023-11-06 20:05:14 +0000

    move avatar on profile view

--- src/routes/profiles/[id]/+page.svelte
+++ src/routes/profiles/[id]/+page.svelte
@@ -12,18 +12,18 @@
 <h3>{profile.first_name} {profile.last_name}</h3>
 
 <p>
-	<Avatar
-		{supabase}
-		bind:url={profile.avatar_url}
-		size={10}
-	/>
-</p>
-
-<p>
     {#if profile.website && profile.website !== ""}
         <a href={profile.website}>Website</a>
     {/if}
     {#if profile.orcid_id && profile.orcid_id !== ""}
         <a href={profile.orcid_id}>ORCID</a>
     {/if}
+</p>
+
+<p>
+	<Avatar
+		{supabase}
+		bind:url={profile.avatar_url}
+		size={10}
+	/>
 </p>

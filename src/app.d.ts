a677ea627203f957dd3fb863317ae723e3303c39
Steven Clontz <steven.clontz@gmail.com>
2023-11-11 02:53:02 +0000

    add getProfile, simplify locals.supabase

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,11 +1,12 @@
 import { SupabaseClient, Session } from '@supabase/supabase-js'
-import { Database } from './DatabaseDefinitions'
+import { Database } from './types'
 
 declare global {
 	namespace App {
 		interface Locals {
 			supabase: SupabaseClient<Database>
 			getSession(): Promise<Session | null>
+			getProfile(): Promise<Database["public"]["Tables"]["profiles"]["Row"] | null>
 		}
 		interface PageData {
 			session: Session | null
--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -5,27 +5,27 @@ import type { Database } from './types'
 
 export const handle: Handle = async ({ event, resolve }) => {
   event.locals.supabase = createServerClient<Database>(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY, {
-    cookies: {
-      get: (key) => event.cookies.get(key),
-      set: (key, value, options) => {
-        event.cookies.set(key, value, options)
-      },
-      remove: (key, options) => {
-        event.cookies.delete(key, options)
-      },
-    },
+    cookies: event.cookies
   })
 
-  /**
-   * a little helper that is written for convenience so that instead
-   * of calling `const { data: { session } } = await supabase.auth.getSession()`
-   * you just call this `await getSession()`
-   */
   event.locals.getSession = async () => {
     const {
       data: { session },
     } = await event.locals.supabase.auth.getSession()
     return session
+  }
+
+  event.locals.getProfile = async () => {
+    const session = await event.locals.getSession()
+    if (session) {
+      const { data: profile  } = await event.locals.supabase
+        .from('profiles')
+        .select()
+        .eq('id', session.user.id)
+        .single() 
+      return profile
+    }
+    return null
   }
 
   return resolve(event, {

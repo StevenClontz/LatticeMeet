4c41f83646c6725a67a75734369a8d9c69d429e6
Steven Clontz <steven.clontz@gmail.com>
2024-05-22 09:32:46 -0500

    edit front page

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -4,19 +4,17 @@
 
 <p>
 	<strong>What's this?</strong>
-	{appTitle} is an early preview of an upcoming 
+	{appTitle} is an upcoming 
 	<a href="https://en.wikipedia.org/wiki/Science_gateway">
 		science gateway
 	</a>
-	designed for mathematicians. It is currently being piloted in support of
+	designed for mathematicians. It was piloted in support of
 </p>
 <p style="text-align:center">
-	<a href="/collections/e8ad2fb5-8824-4b18-abec-4c9743b51bdf">
 		The 57th Spring Topology and Dynamics Conference
-	</a>
 </p>
 <p>
-	to be held in March 2024. Contact <a href="mailto:sclontz@southalabama.edu">Steven Clontz</a>
+	held in March 2024. Contact <a href="mailto:sclontz@southalabama.edu">Steven Clontz</a>
 	for more information.
 </p>
 
